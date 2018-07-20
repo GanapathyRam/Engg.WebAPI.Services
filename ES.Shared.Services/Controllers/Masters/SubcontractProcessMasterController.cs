4bfd0393c096131ee08d3e5168332f2d658b432f
jenkyjeni <jeni_edi@yahoo.co.in>
2018-07-20 22:01:49 +0530

    Added FIles for Process sub-contract

---
+++ ES.Shared.Services/Controllers/Masters/SubcontractProcessMasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ES.Shared.Services.Controllers.Masters
+{
+    public class SubcontractProcessMasterController : Controller
+    {
+        // GET: SubcontractProcessMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
