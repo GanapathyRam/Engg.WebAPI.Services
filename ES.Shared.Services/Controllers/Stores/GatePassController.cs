f2cba387fc926887aba90ba3ee1f9b4930cdf868
GANAPATHY <GANAPATHY@GANAPATHY-PC>
2019-02-23 21:25:00 +0530

    Changes : Fetch Gate Pass Receiving

--- ES.Shared.Services/Controllers/Stores/GatePassController.cs
+++ ES.Shared.Services/Controllers/Stores/GatePassController.cs
@@ -311,7 +311,6 @@ namespace ES.Shared.Services.Controllers.Stores
 
             return response;
         }
-
         [HttpPost]
         public GPReceivingResponseDto SaveGPReceivingtDetails(GPReceivingRequestDto GPReceivingRequestDto)
         {
@@ -342,6 +341,37 @@ namespace ES.Shared.Services.Controllers.Stores
             }
 
             return getGPReceivingResponseDto;
+        }
+
+        public GetGPReceivedDetailsResponseDto GetGPReceivedDetails()
+        {
+            GetGPReceivedDetailsResponseDto response = new GetGPReceivedDetailsResponseDto();
+            try
+            {
+                response = reportGatePass.GetGPReceivedDetails();
+                response.ServiceResponseStatus = 1;
+            }
+            catch (SSException applicationException)
+            {
+                response = new GetGPReceivedDetailsResponseDto
+                {
+                    ServiceResponseStatus = 0,
+                    ErrorMessage = applicationException.Message,
+                    ErrorCode = applicationException.ExceptionCode
+                };
+
+            }
+            catch (Exception exception)
+            {
+                response = new GetGPReceivedDetailsResponseDto
+                {
+                    ServiceResponseStatus = 0,
+                    ErrorCode = ExceptionAttributes.ExceptionCodes.InternalServerError,
+                    ErrorMessage = exception.Message
+                };
+            }
+
+            return response;
         }
 
     }
